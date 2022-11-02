406aad37cbffa81bac425663133f991a35b06f1b
Rishi-2905 <108079408+Rishi-2905@users.noreply.github.com>
2022-11-02 15:43:04 +0530

    Add files via upload
    it's not a binary search , passing an array as a parameter to outer/another class and perform sorting operation and return an array.

---
+++ Binarysearch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Binarysearch{
+    static int a=10,b=10;
+       static class person{
+            public static void check()
+    
+            {
+                int c=a+b;
+                System.out.println(c);
+            }
+        }
+   public static void main(String[] args) {
+    //  int arr[]={32,4,36,76,12,43};  
+    //  int len=arr.length;
+        bus b=new bus.person();
+        b.check();
+    }
+}
